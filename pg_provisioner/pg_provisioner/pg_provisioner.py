e8888fb60bce9f97a6541bef38cc5bb4fcfc5706
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-05-20 14:45:15 +0300

    fix(pg_provisioner): try solve syntax problem

--- pg_provisioner/pg_provisioner/pg_provisioner.py
+++ pg_provisioner/pg_provisioner/pg_provisioner.py
@@ -56,7 +56,7 @@ class PgProvisioner:
                 '-i /home/student/PostgreSQL-Ansible-Automation/ansible/inventories/ '
                 '/home/student/PostgreSQL-Ansible-Automation/ansible/provision_postgresql_VM.yml '
                 '--vault-password-file /home/student/.vault_pass '
-                f'-e "{{\\"vm_name_user_input\\":\\"{name}\\",\\"vm_id\\":\\"{vm_id}\\"}}"'),
+                f'-e vm_name_user_input="{name}" -e vm_id="{vm_id}"'),
             input_data=f"{self.vault_password}\n"
         )
 
