d7c0d382a7942ca5904cd3f5ee5373743da8f756
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-05-28 17:27:44 +0300

    update(models): update the models from models.py in order to define a solid architeture

--- django_rest_api/provisioner_api/models.py
+++ django_rest_api/provisioner_api/models.py
@@ -4,43 +4,140 @@ from django.db import models
 
 class PostgreSQLVM(models.Model):
 
+    """ Represents a VM provisioned with PostgreSQL."""
+
+    # ATTRIBUTES
+
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=200, unique=True)
-    hostname = models.CharField(max_length=200, unique=True)
-    ip_address = models.GenericIPAddressField(unique=True, blank=True, null=True)
+    vm_name = models.CharField(max_length=200)
+    base_vm_name = models.CharField(max_length=200)
+    ipv4_address = models.GenericIPAddressField(unique=True, blank=True, null=True)
     status = models.CharField(max_length=200, default='Started')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    # GETTERS
+    @property
+    def hostname(self):
+        return f"{self.base_vm_name}-{self.vm_name}"
+
+    # SETTERS
+
+    # OPERATORS
+
     def __str__(self):
-        return self.name
+        """Returns the hostname of this VM."""
+        return self.hostname
+
+    # CONSTRAINTS
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['vm_name', 'base_vm_name'],
+                name='unique_hostname_constraint',
+            ),
+        ]
+
+class PostgreSQLInstance(models.Model):
+    """Represents PostgreSQL cluster."""
+
+    # ATTRIBUTES
+
+    id = models.AutoField(primary_key=True)
+    port = models.IntegerField(default=5432)
+    version = models.CharField(max_length=10)
+    vm = models.ForeignKey('PostgreSQLVM', on_delete=models.CASCADE)
+
+    # SETTERS
+
+    # OPERATORS
+
+    def __str__(self):
+        return f"VM: {self.vm}; Instance ID: {self.id}; Port: {self.port}"
+
+    # CONSTRAINTS
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['vm', 'port'],
+                name='unique_port_per_vm',
+            )
+        ]
 
 class PostgreSQLDatabase(models.Model):
 
+    """Represents the PostgreSQL database."""
+
+    # ATTRIBUTES
+
     id = models.AutoField(primary_key=True)
-    vm_id = models.IntegerField()
-    name = models.CharField(max_length=200, unique=True)
-    owner = models.CharField(max_length=200, unique=True)
+    db_name = models.CharField(max_length=200)
+    instance = models.ForeignKey('PostgreSQLInstance', on_delete=models.CASCADE)
+    owner = models.ForeignKey('PostgreSQLUser', on_delete=models.CASCADE)
     encoding = models.CharField(max_length=200)
     collate = models.CharField(max_length=200)
     ctype = models.CharField(max_length=200)
     access_privileges = models.CharField(max_length=200)
-    size = models.CharField(max_length=200)
+    size = models.CharField(max_length=200) # this needs adjustment
     tablespace = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
+    # GETTERS
+
+    # SETTERS
+
+    # OPERATORS
+
+    def __str__(self):
+        return f"PostgreSQL Instance: {self.instance}; DB_name: {self.db_name}; Owner: {self.owner}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['db_name', 'instance'],
+                name='unique_name_instance_owner',
+            )
+        ]
+
 class PostgreSQLBackup(models.Model):
 
+    """Represents the PostgreSQL backup."""
+
+    # ATTRIBUTES
+
     id = models.AutoField(primary_key=True)
-    database_id = models.IntegerField()
+    instance = models.ForeignKey('PostgreSQLInstance', on_delete=models.CASCADE)
     type = models.CharField(max_length=200)
     stanza = models.CharField(max_length=200)
-    backup_id = models.CharField(max_length=200)
     start_at = models.DateTimeField()
     stop_at = models.DateTimeField()
     wal_start = models.CharField(max_length=200)
     wal_stop = models.CharField(max_length=200)
-    cluster_size = models.CharField(max_length=200)
-    cluster_backup_size = models.CharField(max_length=200)
-    backup_size = models.CharField(max_length=200)
-    backup_set_size = models.CharField(max_length=200)
+    cluster_size = models.CharField(max_length=200) # needs adjustment
+    cluster_backup_size = models.CharField(max_length=200) # needs adjustment
+    backup_size = models.CharField(max_length=200) # needs adjustment
+    backup_set_size = models.CharField(max_length=200) # needs adjustment
+
+    # GETTERS
+
+    # SETTERS
+
+    # OPERATORS
+
+    def __str__(self):
+        return f"PostgreSQL Instance: {self.instance} Backup ID: {self.id}"
+
+class PostgreSQLUser(models.Model):
+    """Represents the PostgreSQL user."""
+
+    # ATTRIBUTES
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, unique=True)
+
+    # OPERATORS
+
+    def __str__(self):
+        return f"User: {self.name}"
