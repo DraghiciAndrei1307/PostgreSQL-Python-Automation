6fd7823647b648ed085692df130efaf9347bbdad
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-06-05 14:12:10 +0300

    lint(os_runner): add module docstring inside __init__.py

--- os_runner/os_runner/__init__.py
+++ os_runner/os_runner/__init__.py
@@ -1,1 +1,6 @@
+"""
+    OS Executor module for running and managing operating system commands.
+    This package exposes the core OsRunner class for easier imports.
+"""
+
 from .os_runner import OsRunner
