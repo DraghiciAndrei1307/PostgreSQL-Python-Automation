6fd8e6e8fa04644b5aea2edd7adfa5ecf9710b8d
Drăghici Andrei <draghici.andrei12@yahoo.com>
2026-07-22 13:01:17 +0300

    fix(celery.py): use the official configuration of Celery 'django.conf:settings'

--- django_rest_api/django_api/celery.py
+++ django_rest_api/django_api/celery.py
@@ -8,6 +8,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_api.settings')
 app = Celery('django_api')
 app.conf.enable_utc = False
 
-app.config_from_object(settings, namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace='CELERY')
 
 app.autodiscover_tasks()
